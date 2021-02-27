8961bd3a0f8e8e80d01d64cf4ab3e7c2f448fdc7
Christian Farris <christianfarriscs@gmail.com>
2021-02-26 18:58:57 -0600

    rip out unneeded configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,8 @@
 module.exports = {
   extends: [
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended', // Use recommended rules from @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable conficting rules
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. MUST ALWAYS BE LAST.
   ],
-  parser: '@typescript-eslint/parser', // Use TS Parser
   parserOptions: {
     ecmaFeatures: {
       jsx: true, // Allows for the parsing of JSX
@@ -20,11 +17,6 @@ module.exports = {
     'unicorn/no-abusive-eslint-disable': 'off',
   },
   settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.ts', '.tsx'],
-      },
-    },
     react: {
       version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
     },
--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped configured with ESLint, Prettier, Jest, RTL, TypeScript and Emotion.
-
-This boilerplate code was last edited on 2/7/21 by [@chrstnfrrs](https://github.com/chrstnfrrs).
+This is the documentation for [Aspire Components](https://www.aspirecomponents.com/).
 
 ## Get Started
 
@@ -10,6 +8,3 @@ yarn dev
 ```
 [http://localhost:3000](http://localhost:3000)
 
-## Use cases
-
-[Erin Tallard Fitness](https://github.com/chrstnfrrs/etf-v3/tree/main/web)

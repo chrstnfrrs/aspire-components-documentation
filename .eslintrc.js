9cbee01b8eeae917cfd242fdab119acaca22c862
Christian Farris <christianfarriscs@gmail.com>
2021-03-20 23:30:49 -0500

    config import/resolver, turn off no param reassign for mutate in reduce, allow index key

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,10 +12,18 @@ module.exports = {
   rules: {
     'eslint-comments/no-unlimited-disable': 'off',
     'no-use-before-define': 'off', // React imports have an error to due version mismatches in packages
+    'no-param-reassign': 'off', // Reduce functions
     'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.jsx'] }], // Allow JSX is TSX Files
+    'react/no-array-index-key': 'off', // :shrug:
     'unicorn/no-abusive-eslint-disable': 'off',
   },
   settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.types.ts', '.ts', 'tsx', 'js', '.jsx'],
+      },
+      typescript: {},
+    },
     react: {
       version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
     },

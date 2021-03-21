055376f5aadee1dc691abce8c8b888a9b93aa4f0
Christian Farris <christianfarriscs@gmail.com>
2021-03-20 23:35:59 -0500

    Create project types file

---
+++ types.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+// @ts-ignore
+type ALLOW_ANY = any;
+/* eslint-enable */
+
+interface IComponentInfo {
+  name: string;
+  category: string;
+}
+
+export type { ALLOW_ANY, IComponentInfo };

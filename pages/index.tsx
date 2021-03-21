0a8b5fe90a52726177cc3dce73faf75b9e5ad4df
Christian Farris <christianfarriscs@gmail.com>
2021-03-20 23:38:42 -0500

    create home page

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { PlaceholderContainer } from '../components/placeholders';
+
+const HomePage: React.FC = () => (
+  <PlaceholderContainer>
+    <strong>{'Aspire Components'}</strong>
+    <p>{'Powered By Sanity.io'}</p>
+  </PlaceholderContainer>
+);
+
+export default HomePage;

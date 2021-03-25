6ba340c906ea46d8cd9cc04ef0e0bf06f08a368e
Christian Farris <christianfarriscs@gmail.com>
2021-03-24 21:16:01 -0500

    create convertKebabToPascal

--- utils/convert-case.ts
+++ utils/convert-case.ts
@@ -4,10 +4,16 @@ const convertPascalToKebab = (str: string): string => {
   return `${lowercaseStr.slice(0, 1)}-${lowercaseStr.slice(1)}`;
 };
 
+const convertKebabToPascal = (str: string): string => {
+  const lowercaseStr = str.toLowerCase();
+
+  return `${lowercaseStr.slice(0, 1).toUpperCase()}${lowercaseStr.slice(2, 3).toUpperCase()}${lowercaseStr.slice(3)}`;
+};
+
 const capitalizeString = (str: string): string => {
   const lowercaseStr = str.toLowerCase();
 
   return `${lowercaseStr.slice(0, 1).toUpperCase()}${lowercaseStr.slice(1)}`;
 };
 
-export { convertPascalToKebab, capitalizeString };
+export { convertPascalToKebab, convertKebabToPascal, capitalizeString };
